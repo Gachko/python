fdb4334c4d5acbe5cb71c71d902622c4b7ad2826
kgachko <k.hachko@andersenlab.com>
2024-10-25 00:09:46 +0200

    add auth

--- rssReader/urls.py
+++ rssReader/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls")),
     path("", include("channel.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/authapp/", include("authapp.urls")),
 ]
